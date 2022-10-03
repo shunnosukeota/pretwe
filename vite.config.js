6d0659de763ed304754487cb8478ac1f422595c5
shun <mr2kfhtmri@i.softbank.jp>
2022-10-03 17:01:33 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { version } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                
+            ],
+            refresh: true,
+        }),
+    ],
+});
