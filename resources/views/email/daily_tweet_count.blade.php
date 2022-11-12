61687ca48d4ba7af41a62bdde0f8ad71fd91bed5
shunnosukeota <mr2kfhtmri@i.softbank.jp>
2022-11-12 11:31:27 +0900

    AWS

--- resources/views/email/daily_tweet_count.blade.php
+++ resources/views/email/daily_tweet_count.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
 
-# 昨日は{{ $count }}件のつぶやきが追加されました！
+# 昨日は{{ $count }}件の投稿が追加されました！
 
 {{ $toUser->name }}さんこんにちは！
 
-昨日は{{ $count }}件のつぶやきが追加されましたよ！最新のつぶやきを見に行きましょう。
+昨日は{{ $count }}件の投稿が追加されましたよ！最新のつぶやきを見に行きましょう。
 
 @component('mail::button', ['url' => route('tweet.index')])
     投稿を見に行く
